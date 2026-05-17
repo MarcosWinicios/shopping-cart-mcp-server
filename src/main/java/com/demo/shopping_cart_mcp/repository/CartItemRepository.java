b2c47eec1b717a3a9a0be683f0d2b456aeca0e12
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2026-05-17 01:13:30 -0300

    feat: Adjust repository

--- src/main/java/com/demo/shopping_cart_mcp/repository/CartItemRepository.java
+++ src/main/java/com/demo/shopping_cart_mcp/repository/CartItemRepository.java
@@ -1,10 +1,12 @@
 package com.demo.shopping_cart_mcp.repository;
 
 import com.demo.shopping_cart_mcp.entity.CartItem;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     CartItem findByProductId(String productId);
+    @Transactional
     void deleteByProductId(String productId);
 }
